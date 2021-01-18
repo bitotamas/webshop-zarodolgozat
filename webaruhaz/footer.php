9e84e1adfc9be2ca3d87c0165456567774028795
bitotamas <bito.tamas51@gmail.com>
2021-01-18 12:54:02 +0100

    footer

--- webaruhaz/footer.php
+++ webaruhaz/footer.php
@@ -1,8 +1,9 @@
 <footer class="py-4 bg-danger text-white">
         <div class="text-center">
+        <a href="#top" class="btn-info rounded">Vissza a lap tetejére</a><br>
             <small>
-                MagmaTech Számítéstechnikai Webáruház<br> 
-                6000 Kecskemét Valamilyen utca 69.<br> 
+                
+                MagmaTech Számítéstechnikai Webáruház | 6000 Kecskemét Valamilyen utca 69.<br> 
                 Telefon: +36 55 555-5555 <br> 
                 Email: emailkuldesteszt@gmail.com
 
